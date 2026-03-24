ba63c97695327c5f3cae2f1a1562700b3c5929f4
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2026-03-23 23:41:30 -0500

    Remove redudant attributes from vehicle cards

--- Assets/Scripts/Cards/Data/VehicleDataSO.cs
+++ Assets/Scripts/Cards/Data/VehicleDataSO.cs
@@ -14,14 +14,10 @@ namespace AcceleracersCCG.Cards.Data
         [Header("Vehicle")]
         public Team team;
         public ModabilityIcon modabilityIcons;
-        public bool isAdvancedVehicle;
-        public string baseVehicleName;
-
         public override CardData ToCardData()
         {
             return new VehicleCardData(cardId, cardName, new SPP(speed, power, performance),
                 team: team, modabilityIcons: modabilityIcons, terrainIcons: terrainIcons,
-                isAdvancedVehicle: isAdvancedVehicle, baseVehicleName: baseVehicleName,
                 effectIds: effectIds);
         }
     }
--- Assets/Scripts/Cards/VehicleCardData.cs
+++ Assets/Scripts/Cards/VehicleCardData.cs
@@ -9,23 +9,17 @@ namespace AcceleracersCCG.Cards
         public SPP SPP { get; }
         public Team Team { get; }
         public ModabilityIcon ModabilityIcons { get; }
-        public bool IsAdvancedVehicle { get; }
-        public string BaseVehicleName { get; }
 
         public VehicleCardData(string id, string name, SPP spp,
             Team team = Team.None,
             ModabilityIcon modabilityIcons = ModabilityIcon.None,
             TerrainIcon terrainIcons = TerrainIcon.None,
-            bool isAdvancedVehicle = false,
-            string baseVehicleName = null,
             IEnumerable<string> effectIds = null)
             : base(id, name, effectIds: effectIds, terrainIcons: terrainIcons)
         {
             SPP = spp;
             Team = team;
             ModabilityIcons = modabilityIcons;
-            IsAdvancedVehicle = isAdvancedVehicle;
-            BaseVehicleName = baseVehicleName;
         }
     }
 }
